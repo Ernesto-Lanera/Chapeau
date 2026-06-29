44a0d7c37360ee65ff62b81e3bb4f089150fd73e
CarloLINH <726385@student.inholland.nl>
2026-06-29 12:35:59 +0200

    dode code weg, confirmation

--- Chapeau/Controllers/PaymentController.cs
+++ Chapeau/Controllers/PaymentController.cs
@@ -76,13 +76,7 @@ namespace Chapeau.Controllers
             }
         }
 
-        public IActionResult Confirmation(int orderId, decimal amount, string method)
-        {
-            ViewBag.OrderId = orderId;
-            ViewBag.Amount = amount;
-            ViewBag.Method = method;
-            return View();
-        }
+        
 
         private PaymentOrderViewModel BuildPaymentViewModel(int orderId, int tableNumber)
         {
